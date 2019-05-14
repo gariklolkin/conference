b6530f2229c628b23674b5914df40673dc610c16
iabashkin <IAbashkin@kyriba.com>
2019-05-15 01:01:51 +0300

    Registration service: add Spring REST Docs

--- registration/src/test/java/com/kyriba/registration/api/ChangeTicketOwnerApiTest.java
+++ registration/src/test/java/com/kyriba/registration/api/ChangeTicketOwnerApiTest.java
@@ -1,16 +1,23 @@
 package com.kyriba.registration.api;
 
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
 import static junit.framework.TestCase.assertNotNull;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
 
 
 @RunWith(SpringRunner.class)
@@ -19,11 +26,23 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 public class ChangeTicketOwnerApiTest
 {
 
+  @Rule
+  public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+  private RequestSpecification documentationSpec;
+
+  @Before
+  public void setUp() {
+    documentationSpec = new RequestSpecBuilder()
+        .addFilter(documentationConfiguration(restDocumentation)).build();
+  }
+
   @Test
   public void attendeeCanChangeTicketsOwner()
   {
-    String ticketId = given()
+    String ticketId = given(documentationSpec)
         .contentType(APPLICATION_JSON_UTF8_VALUE)
+        .filter(document("api/v1/tickets/exchange"))
         .body("{\n" +
             "  \"ticketOwner\": \"1234\"\n" +
             "}")
--- registration/src/test/java/com/kyriba/registration/api/RegistrationApiTest.java
+++ registration/src/test/java/com/kyriba/registration/api/RegistrationApiTest.java
@@ -1,16 +1,23 @@
 package com.kyriba.registration.api;
 
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
 import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static io.restassured.RestAssured.given;
 import static junit.framework.TestCase.assertNotNull;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
 
 
 @RunWith(SpringRunner.class)
@@ -18,11 +25,23 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 @AutoConfigureMockMvc
 public class RegistrationApiTest
 {
+  @Rule
+  public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+  private RequestSpecification documentationSpec;
+
+  @Before
+  public void setUp() {
+    documentationSpec = new RequestSpecBuilder()
+        .addFilter(documentationConfiguration(restDocumentation)).build();
+  }
+
   @Test
   public void attendeeCanBeRegistered()
   {
-    String attendeeId = given()
+    String attendeeId = given(documentationSpec)
         .contentType(APPLICATION_JSON_UTF8_VALUE)
+        .filter(document("api/v1/attendees/registration"))
         .body("{\n" +
             "  \"firstName\": \"Ilya\",\n" +
             "  \"lastName\": \"Abashkin\",\n" +
@@ -52,8 +71,9 @@ public class RegistrationApiTest
   @Test
   public void attendeeCanSeeRegistrationStatus()
   {
-    String status = given()
+    String status = given(documentationSpec)
         .contentType(APPLICATION_JSON_UTF8_VALUE)
+        .filter(document("api/v1/attendees/status"))
 
         .when()
         .get("/api/v1/attendees/123456789/status")
