d896a50ca37fdc05642d5e7c9300c083d7150791
asamal <asamal@kyriba.com>
2019-05-16 19:14:21 +0300

    Sponsorship: package structure updated

--- sponsorship/src/main/java/com/kyriba/conference/sponsorship/configuration/SwaggerConfiguration.java
+++ sponsorship/src/main/java/com/kyriba/conference/sponsorship/configuration/SwaggerConfiguration.java
@@ -29,7 +29,9 @@ public class SwaggerConfiguration
   @Bean
   public Docket productApi()
   {
-    return new Docket(DocumentationType.SWAGGER_2).select()
-        .apis(RequestHandlerSelectors.basePackage("com.kyriba.training.sponsorship.api")).build();
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.kyriba.conference.sponsorship.api"))
+        .build();
   }
 }
