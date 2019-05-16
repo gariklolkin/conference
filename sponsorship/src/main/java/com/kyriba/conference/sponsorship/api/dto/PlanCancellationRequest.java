d38eadf281131d4ab86bea526275efec11ea82f5
asamal <asamal@kyriba.com>
2019-05-16 19:48:17 +0300

    Sponsorship: removed redundant javadoc

--- sponsorship/src/main/java/com/kyriba/conference/sponsorship/api/dto/PlanCancellationRequest.java
+++ sponsorship/src/main/java/com/kyriba/conference/sponsorship/api/dto/PlanCancellationRequest.java
@@ -1,13 +1,3 @@
-/****************************************************************************
- * Copyright 2000 - 2019 Kyriba Corp. All Rights Reserved.                  *
- * The content of this file is copyrighted by Kyriba Corporation            *
- * and can not be reproduced, distributed, altered or used in any form,     *
- * in whole or in part.                                                     *
- *                                                                          *
- * Date          Author         Changes                                     *
- * 2019-05-13    M-ASL          Created                                     *
- *                                                                          *
- ****************************************************************************/
 package com.kyriba.conference.sponsorship.api.dto;
 
 import lombok.AllArgsConstructor;
