4178f273ef757f21748d3a87631c25dfcd72634e
SSH <sshvaiko@kyriba.com>
2019-05-20 11:34:44 +0300

    Adding Spring Doc

--- notification/src/test/java/com/kyriba/training/notification/api/EmailNotificationControllerTest.java
+++ notification/src/test/java/com/kyriba/training/notification/api/EmailNotificationControllerTest.java
@@ -5,24 +5,48 @@ import com.kyriba.training.notification.api.dto.MessageStatus;
 import com.kyriba.training.notification.api.dto.MessageType;
 import com.kyriba.training.notification.api.dto.NotificationStatus;
 import com.kyriba.training.notification.api.dto.Recipient;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.apache.http.HttpStatus;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static junit.framework.TestCase.assertEquals;
-import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
 import static io.restassured.RestAssured.given;
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertNotNull;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
+import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class EmailNotificationControllerTest
 {
+  @Rule
+  public final JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+  private RequestSpecification documentationSpec;
+
+  @Before
+  public void setUp() {
+    this.documentationSpec = new RequestSpecBuilder()
+        .addFilter(documentationConfiguration(restDocumentation))
+        .build();
+  }
+
+
+
   @Test
   public void emailNotificationShouldBeSent()
   {
@@ -32,10 +56,10 @@ public class EmailNotificationControllerTest
     EmailNotificationMessage emailNotificationMessage = EmailNotificationMessage.builder().recipients(recipients).body("Email Body").subject("Subject").build();
 
 
-    NotificationStatus status = given()
+    NotificationStatus status = given(this.documentationSpec)
         .contentType(APPLICATION_JSON_UTF8_VALUE)
-//    {"recipients":[{"name":"Name1","address":"Address1"},{"name":"Name2","address":"Address2"}],"body":"Email Body","subject":"Subject","emailMessageOptions":null}
         .body(emailNotificationMessage)
+        .filter(document("request", requestFields(fieldWithPath("api"))))
         .when().post("/api/notification/email/notify")
 
         .then()
