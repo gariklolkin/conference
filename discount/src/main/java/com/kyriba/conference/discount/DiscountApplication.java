0929a9d4625d534df0597f01ed6e9998356b0222
Igor Lizura <ilizura@kyriba.com>
2019-07-08 17:21:55 +0300

    the eureka client was added

--- discount/src/main/java/com/kyriba/conference/discount/DiscountApplication.java
+++ discount/src/main/java/com/kyriba/conference/discount/DiscountApplication.java
@@ -2,8 +2,10 @@ package com.kyriba.conference.discount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DiscountApplication {
 
 	public static void main(String[] args) {
