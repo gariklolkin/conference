05f5297212f10f894ea44db5a2ffa75308828f27
asamal <asamal@kyriba.com>
2019-12-17 13:36:39 +0300

    Sponsorship: first contract, consumer side updated

---
+++ sa-gateway/src/test/java/com/kyriba/conference/gateway/GatewayApplicationTest.java
@@ -0,0 +1,48 @@
+package com.kyriba.conference.gateway;
+
+import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.contract.stubrunner.spring.AutoConfigureStubRunner;
+import org.springframework.cloud.contract.stubrunner.spring.StubRunnerProperties;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+/**
+ * @author Aliaksandr Samal
+ */
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureStubRunner(ids = { "com.kyriba.training:sponsorship:+:stubs:8080" },
+    stubsMode = StubRunnerProperties.StubsMode.LOCAL)
+@ActiveProfiles("test")
+class GatewayApplicationTest
+{
+  @Test
+  void registerSponsor()
+  {
+    //
+    String id = RestAssured
+        .given()
+          .contentType(MediaType.APPLICATION_JSON_VALUE)
+          .body("{\n" +
+            "  \"name\": \"Aaa Bbb\" ,\n" +
+            "  \"email\": \"a@b.org\"\n" +
+            "}")
+        .when()
+          .post("/api/v1/sponsors")
+        .then()
+          .statusCode(HttpStatus.SC_OK)
+          .contentType(MediaType.APPLICATION_JSON_VALUE)
+          .extract()
+          .jsonPath()
+          .get("id");
+    assertNotNull(id);
+  }
+}
