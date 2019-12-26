bf349e31ebe13aa0ca9d2600fbf036a6e8d205b9
asamal <asamal@kyriba.com>
2019-12-26 21:44:45 +0300

    Global: Eureka replaced with Kubernetes service discovery

--- sponsorship/src/main/java/com/kyriba/conference/sponsorship/SponsorshipApplication.java
+++ sponsorship/src/main/java/com/kyriba/conference/sponsorship/SponsorshipApplication.java
@@ -3,11 +3,11 @@ package com.kyriba.conference.sponsorship;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 @EnableConfigurationProperties
